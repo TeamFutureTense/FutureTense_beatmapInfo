12239aac02e7fb85c563d5fefca3265060e211bb
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2025-02-19 10:59:05 -0500

    Add basic data fetching and display logic

--- static/beatmapInfo/index.js
+++ static/beatmapInfo/index.js
@@ -1,6 +1,7 @@
 // connecting to websocket
 import WebSocketManager from './js/socket.js';
 import anime from './js/anime.js';
+// import axiosMin from './js/axios.min.js';
 const socket = new WebSocketManager('127.0.0.1:24050');
 
 // cache values here to prevent constant updating
@@ -8,20 +9,35 @@ const cache = {
   name: "Unknown",
   difficulty: 0,
   length: 0,
-  diffName: "Unknown diff"
+  diffName: "Unknown diff",
+  userid: ""
 };
 
+function formatTime(ms) {
+  const totalSeconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
 // receive message update from websocket
-socket.api_v2(({ play, state, performance, resultsScreen, beatmap }) => {
+socket.api_v2(({ state, beatmap, profile }) => {
   try {
-    if (state.name === "") {
+    if (state.name === "play") {
       // get data
       cache.name = beatmap.title;
       cache.difficulty = beatmap.stats.stars.total;
-      cache.length = beatmap.time.live;
+      cache.diffName = beatmap.version;
+      cache.length = beatmap.time.mp3Length;
+      cache.userid = profile.id;
+      // console.log("Updated cache", cache)
       
       // set styles
-
+      document.getElementById("songTitle").innerText = cache.name;
+      document.getElementById("starRating").innerText = cache.difficulty;
+      document.getElementById("diffName").innerText = cache.diffName;
+      document.getElementById("length").innerText = formatTime(cache.length);
       // display overlay
     }
     else {
